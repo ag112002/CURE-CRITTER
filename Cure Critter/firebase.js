588f0f7cff68df5b93b1fad006039143af72bc5b
AYUSHI GUPTA <89902111+ag112002@users.noreply.github.com>
2022-06-22 16:03:06 +0530

    Add files via upload

---
+++ Cure Critter/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYBhuKqPi9G4Djp4vXe8zvYjHcp5UOtIQ",
+    authDomain: "curecritterfeedback.firebaseapp.com",
+    projectId: "curecritterfeedback",
+    storageBucket: "curecritterfeedback.appspot.com",
+    messagingSenderId: "4165529555",
+    appId: "1:4165529555:web:71b08620f1f9b59b9b4926"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
---
+++ Cure Critter/preloader.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  const pre = document.getElementById('preloader');
+
+  
+  pre.style.display = 'none';
+
+ 
+}, 1000); 
